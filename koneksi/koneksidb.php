d0864d95935dd584ca8b452b5742da1b89486d6f
luckitalulu <51027747+luckitalulu@users.noreply.github.com>
2019-06-13 20:44:23 +0700

    update password

--- koneksi/koneksidb.php
+++ koneksi/koneksidb.php
@@ -1,5 +1,5 @@
 <?php
 
-     $conn = mysqli_connect("cellin.database.windows.net", "cellinlogin", "", "cellin");
+     $conn = mysqli_connect("cellin.database.windows.net", "cellinlogin", "CellInLogin123", "cellin");
 
 ?>
