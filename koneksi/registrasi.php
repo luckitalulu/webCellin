63cd0c986f219a16ae6d956b24be5997aec83fce
luckitalulu <51027747+luckitalulu@users.noreply.github.com>
2019-06-14 11:01:16 +0700

    Update registrasi.php

--- koneksi/registrasi.php
+++ koneksi/registrasi.php
@@ -2,44 +2,45 @@
 $response = array();
 include 'db/db_connect.php';
 include 'functions.php';
- 
+
 //Get the input request parameters
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE); //convert JSON into array
- 
+
 //Check for Mandatory parameters
-if(isset($input['username']) && isset($input['password']) && isset($input['full_name'])){
+if(isset($input['username']) && isset($input['password'])){
 	$username = $input['username'];
 	$password = $input['password'];
-	$fullName = $input['full_name'];
-	
-	//Check if user already exist
-	if(!userExists($username)){
- 
-		//Get a unique Salt
-		$salt         = getSalt();
-		
-		//Generate a unique password Hash
-		$passwordHash = password_hash(concatPasswordWithSalt($password,$salt),PASSWORD_DEFAULT);
-		
-		//Query to register new user
-		$insertQuery  = "INSERT INTO member(username, full_name, password_hash, salt) VALUES (?,?,?,?)";
-		if($stmt = $con->prepare($insertQuery)){
-			$stmt->bind_param("ssss",$username,$fullName,$passwordHash,$salt);
-			$stmt->execute();
-			$response["status"] = 0;
-			$response["message"] = "User created";
-			$stmt->close();
+	$query    = "SELECT full_name,password_hash, salt FROM member WHERE username = ?";
+
+	if($stmt = $con->prepare($query)){
+		$stmt->bind_param("s",$username);
+		$stmt->execute();
+		$stmt->bind_result($fullName,$passwordHashDB,$salt);
+		if($stmt->fetch()){
+			//Validate the password
+			if(password_verify(concatPasswordWithSalt($password,$salt),$passwordHashDB)){
+				$response["status"] = 0;
+				$response["message"] = "Login successful";
+				$response["full_name"] = $fullName;
+			}
+			else{
+				$response["status"] = 1;
+				$response["message"] = "Invalid username and password combination";
+			}
 		}
-	}
-	else{
-		$response["status"] = 1;
-		$response["message"] = "User exists";
+		else{
+			$response["status"] = 1;
+			$response["message"] = "Invalid username and password combination";
+		}
+		
+		$stmt->close();
 	}
 }
 else{
 	$response["status"] = 2;
 	$response["message"] = "Missing mandatory parameters";
 }
+//Display the JSON response
 echo json_encode($response);
 ?>
