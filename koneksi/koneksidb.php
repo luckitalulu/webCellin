e6cdd8656a44982f46466e1cc505bd7032fbbfc7
luckitalulu <51027747+luckitalulu@users.noreply.github.com>
2019-06-14 10:56:37 +0700

    Update koneksidb.php

--- koneksi/koneksidb.php
+++ koneksi/koneksidb.php
@@ -1,5 +1,15 @@
 <?php
-
-     $conn = mysqli_connect("cellin.database.windows.net", "cellinlogin", "CellIn123", "cellin");
-
+define('DB_USER', "cellinlogin"); // db user
+define('DB_PASSWORD', "CellIn123"); // db password (mention your db password here)
+define('DB_DATABASE', "cellin"); // database name
+define('DB_SERVER', "cellin.database.windows.net"); // db server
+ 
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+ 
+// Check connection
+if(mysqli_connect_errno())
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+ 
 ?>
